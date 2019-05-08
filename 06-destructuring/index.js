0db7a4f9756dea34d0f53c7264745977c99b32c6
Dave Ceddia <dceddia@gmail.com>
2019-05-07 23:47:44 -0400

    Split up before/after code for a few lessons

--- 06-destructuring/index.js
+++ 06-destructuring/index.js
@@ -1,15 +1,8 @@
+// Destructuring arrays and objects
+
 const nums = [1, 2, 3, 4, 5];
 const user = {
   username: 'dceddia',
   firstName: 'Dave',
   lastName: 'Ceddia'
 };
-
-function print({ quiet = true, message = '' }) {
-  if (!quiet) {
-    console.log(message);
-  }
-}
-print({
-  quiet: false
-});
--- 10-filter/index.js
+++ 10-filter/index.js
@@ -1,3 +1,5 @@
+// Using the `filter` method on arrays
+
 const spices = [
   { id: 1, name: 'Black Pepper' },
   { id: 2, name: 'Turmeric' },
@@ -5,19 +7,3 @@ const spices = [
   { id: 4, name: 'Coriander' },
   { id: 5, name: 'Chili Powder' }
 ];
-
-// starting at index 3, delete 1 item
-spices.splice(3, 1);
-console.log(spices);
-
-// then, try the functional form using filter:
-const deleteId = 4;
-const newSpices = spices.filter(spice => {
-  return spice.id !== 4;
-});
-console.log(newSpices);
-console.log(spices === newSpices);
-
-// spices starting with C
-const c = spices.filter(item => item.name.startsWith('C'));
-console.log(c);
