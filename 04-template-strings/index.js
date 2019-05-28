b0b46b7597ef7cc1522810800eeb0fd206532246
Dave Ceddia <dceddia@gmail.com>
2019-05-28 18:15:18 -0400

    Finished Template Strings lesson

--- 04-template-strings/index.js
+++ 04-template-strings/index.js
@@ -3,3 +3,13 @@
 const firstName = 'Dave';
 const lastName = 'Ceddia';
 const usesReact = true;
+
+const fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+const summary = `${fullName} ${usesReact ? 'uses React' : "doesn't use React"}`;
+console.log(summary);
+
+const add = (a, b) => a + b;
+const answer = `The answer is ${add(40, 2)}`;
+console.log(answer);
