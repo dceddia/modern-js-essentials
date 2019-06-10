1a2876507982855d880f5a5d3899c53dec423cca
Dave Ceddia <dceddia@gmail.com>
2019-06-10 18:14:41 -0400

    Finished lesson on map

--- 09-map/index.js
+++ 09-map/index.js
@@ -1,9 +1,26 @@
 const people = [
-  { first: 'Dan', last: 'Abramov', handle: '@dan_abramov' },
-  { first: 'Ali', last: 'Spittel', handle: '@ASpittel' },
-  { first: 'Sarah', last: 'Drasner', handle: '@sarah_edo' }
+  {
+    first: 'Dan',
+    last: 'Abramov',
+    handle: '@dan_abramov'
+  },
+  {
+    first: 'Ali',
+    last: 'Spittel',
+    handle: '@ASpittel'
+  },
+  {
+    first: 'Sarah',
+    last: 'Drasner',
+    handle: '@sarah_edo'
+  }
 ];
 
-// make a list of their names
-
-// add fullName to each person
+const names = people.map((person, index) => {
+  return {
+    ...person,
+    first: person.first + '!!!'
+  };
+});
+console.log(names);
+console.log(people);
